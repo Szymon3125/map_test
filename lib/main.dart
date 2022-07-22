fe839eb10441c6962e0c2f5780a2c2e7ee9a4593
Szymon Głowacki <glowacki@primesoft237.local>
2022-07-22 11:15:18 +0200

    mapbox

--- lib/main.dart
+++ lib/main.dart
@@ -29,54 +29,24 @@ class MapScreen extends StatefulWidget {
   State<MapScreen> createState() => _MapScreenState();
 }
 
-class _MapScreenState extends State<MapScreen>
-    with
-        // ignore: prefer_mixin
-        WidgetsBindingObserver {
+class _MapScreenState extends State<MapScreen> {
   late MapboxMapController mapController;
   late List<LatLng> coordList;
-  bool refreshAllChildrenAfterWakeup = false;
 
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance.addObserver(this);
   }
 
   @override
   Widget build(BuildContext context) {
-    if (refreshAllChildrenAfterWakeup) {
-      refreshAllChildrenAfterWakeup = false;
-      _rebuildAllChildren(context);
-    }
     return Scaffold(
       body: MapboxMap(
         accessToken: const String.fromEnvironment('SDK_REGISTRY_TOKEN'),
         initialCameraPosition: const CameraPosition(target: LatLng(0, 0)),
         onMapCreated: _onMapCreated,
-        onMapClick: (Point<double> point, LatLng latlng) {
-          print('Hello there!');
-        },
       ),
     );
-  }
-
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    if (state == AppLifecycleState.resumed) {
-      setState(() {
-        refreshAllChildrenAfterWakeup = true;
-      });
-    }
-  }
-
-  void _rebuildAllChildren(BuildContext context) {
-    void rebuild(Element element) {
-      element.markNeedsBuild();
-      element.visitChildren(rebuild);
-    }
-
-    (context as Element).visitChildren(rebuild);
   }
 
   Future<void> _onMapCreated(MapboxMapController controller) async {
